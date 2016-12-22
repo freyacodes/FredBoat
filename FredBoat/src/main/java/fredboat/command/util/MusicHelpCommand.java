7eb977c0d13bd5628806f2e336125fcab53f6e12
RealKC <mitrut.e.super@gmail.com>
2016-12-22 18:08:55 +0200

    adds more info to ;;music

--- FredBoat/src/main/java/fredboat/command/util/MusicHelpCommand.java
+++ FredBoat/src/main/java/fredboat/command/util/MusicHelpCommand.java
@@ -56,7 +56,11 @@ public class MusicHelpCommand extends TextCommand {
             ";;shuffle\n" +
             "#Toggles shuffle mode for the current queue.\n" +
             ";;volume <vol>\n" +
-            "#Changes the volume. Values are 0-150 and 100 is the default.```";
+            "#Changes the volume. Values are 0-150 and 100 is the default.\n" +
+            ";;export\n" +
+            "#Export the current queue to a hastebin link, can be later used as a playlist for ;;play.\n" +
+            "@FredBoat♪♪#7284\n" +
+            "Allows interaction with the bot's cleverbot module.```";
 
     public MusicHelpCommand() {
         super(MUSIC);
