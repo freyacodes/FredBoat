a68a44c6abd57ad741b329a8773c0e573c9d1e26
RealKC <mitrut.e.super@gmail.com>
2017-07-01 01:10:13 +0300

    Fixed more stuff, cleaned up imports

--- FredBoat/src/main/java/fredboat/command/maintenance/PingCommand.java
+++ FredBoat/src/main/java/fredboat/command/maintenance/PingCommand.java
@@ -2,15 +2,12 @@ package fredboat.command.maintenance;
 
 import fredboat.commandmeta.abs.Command;
 import fredboat.commandmeta.abs.IMaintenanceCommand;
-import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.*;
 
-import static fredboat.util.BotConstants.FREDBOAT_COLOR;
-
 /**
  * Created by epcs on 6/30/2017.
- * Good enough of an indicator to Discord.
+ * Good enough of an indicator of the ping to Discord.
  */
 
 public class PingCommand extends Command implements IMaintenanceCommand {
@@ -25,7 +22,7 @@ public class PingCommand extends Command implements IMaintenanceCommand {
         JDA jda = guild.getJDA();
         long ping = jda.getPing();
 
-        channel.sendMessage(ping + "ms";).queue();
+        channel.sendMessage(ping + "ms").queue();
     }
 }
 
