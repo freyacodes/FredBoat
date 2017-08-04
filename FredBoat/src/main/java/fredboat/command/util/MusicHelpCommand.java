94e6ebd2097e4f5c850e1d34ab14b8cabb8db530
Donald <3628387+AndrielChaoti@users.noreply.github.com>
2017-08-04 06:01:12 -0600

    Add failure responses.

--- FredBoat/src/main/java/fredboat/command/util/MusicHelpCommand.java
+++ FredBoat/src/main/java/fredboat/command/util/MusicHelpCommand.java
@@ -106,11 +106,11 @@ public class MusicHelpCommand extends Command implements IUtilCommand {
 
     private static void sendCommandsHelpInDM(Guild guild, TextChannel channel, Member invoker, String dmMsg) {
         invoker.getUser().openPrivateChannel().queue(privateChannel ->
-                privateChannel.sendMessage(TextUtils.asMarkdown(dmMsg)).queue( message -> {
-                    // Let the user know they got a DM if it was a success.
-                    /* TODO: Maybe make an I18n string that reflects this.
-                        Something like: Commands Help has been sent to you in a direct message! */
-                    String out = I18n.get(guild).getString("helpSent");
+                privateChannel.sendMessage(TextUtils.asMarkdown(dmMsg)).queue( success -> {
+                    String out = "Commands Help has been sent to your DMs!"; // TODO: I18n
+                    TextUtils.replyWithName(channel, invoker, out);
+                }, failure -> {
+                    String out = ":exclamation:I couldn't send commands help to your DMs! Do you have them turned off?"; // TODO: I18n
                     TextUtils.replyWithName(channel, invoker, out);
                 }));
     }
