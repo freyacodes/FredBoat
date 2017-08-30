1b7f0470b14b4c8d1bcc0341939b68e18c47413e
[RealKC] <[mitrut.e.super@gmail.com]>
2017-08-31 00:20:05 +0300

    Added in order to store lennies and other stuff

--- FredBoat/src/main/java/fredboat/util/AsciiArtConstant.java
+++ FredBoat/src/main/java/fredboat/util/AsciiArtConstant.java
@@ -2,6 +2,43 @@ package fredboat.util;
 
 /**
  * Created by epcs on 8/30/2017.
+ * Do we really consider this to be art? Wouldn't one think this is above the everyday and materialistic thing we call art?
+ * Doesn't this touch your soul in such way that it makes you reconsider life?
+ * Only the untrained soul could consider this to not be beyond art.
  */
-public class AsciiArtConstant {
+public final class AsciiArtConstant {
+
+    public static String MAGICAL_LENNY =
+            //@formatter:off
+            "（ ͡° ͜ʖ ͡°)つ━☆・。\n" +
+            "⊂　　 ノ 　　　・゜+.\n" +
+            "　しーＪ　　　°。+ ´¨)\n" +
+            "　　　　　　　　　.· ´¸.·´¨) ¸.·*¨)\n" +
+            "　　　　　　　　　　(¸.·´ (¸.·' ☆ ABRA KADABRA...";
+            //@formatter:on
+
+            public static String EAGLE_OF_LENNY =
+            //@formatter:off
+            ":＜￣｀ヽ、　　　　　　　／￣＞\n" +
+            "　ゝ、　　＼　／⌒ヽ,ノ 　/´\n" +
+            "　　　ゝ、　`（ ( ͡° ͜ʖ ͡°) ／\n" +
+            "　　 　　>　 　 　,ノ\n" +
+            "　　　　　∠_,,,/´”";
+            //@formatter:on
+
+    public static final String DOG =
+            //Dog created by @Ace#8925
+            //@formatter:off
+            "┈┈╱▏┈┈┈┈┈╱▔▔▔▔╲┈ \n" +
+            "┈┈▏▏┈┈┈┈┈▏╲▕▋▕▋▏ \n" +
+            "┈┈╲╲┈┈┈┈┈▏┈▏┈▔▔▔▆ ------- BORF!\n" +
+            "┈┈┈╲▔▔▔▔▔╲╱┈╰┳┳┳╯ \n" +
+            "╱╲╱╲▏┈┈┈┈┈┈▕▔╰━╯ \n" +
+            "▔╲╲╱╱▔╱▔▔╲╲╲╲┈┈┈ \n" +
+            "┈┈╲╱╲╱┈┈┈┈╲╲▂╲▂┈ \n" +
+            "┈┈┈┈┈┈┈┈┈┈┈╲╱╲╱┈\n" +
+            "Spread him to take over Discord!(Don't spam him though!)";
+             //@formatter:on
 }
+
+//RIP DogCommand.java 2016-2017 "gib pats to me"
