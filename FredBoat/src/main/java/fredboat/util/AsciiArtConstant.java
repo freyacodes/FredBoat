5f76e169fce0ea77cb7cc2f82c6ce721c884e8b5
RealKC <mitrut.e.super@gmail.com>
2017-08-31 13:32:03 +0300

    Did what Shredder suggested

--- FredBoat/src/main/java/fredboat/util/AsciiArtConstant.java
+++ FredBoat/src/main/java/fredboat/util/AsciiArtConstant.java
@@ -8,31 +8,34 @@ package fredboat.util;
  */
 public final class AsciiArtConstant {
 
-    public static String MAGICAL_LENNY =
-            "（ ͡° ͜ʖ ͡°)つ━☆・。\n" +
-            "⊂　　 ノ 　　　・゜+.\n" +
-            "　しーＪ　　　°。+ ´¨)\n" +
-            "　　　　　　　　　.· ´¸.·´¨) ¸.·*¨)\n" +
-            "　　　　　　　　　　(¸.·´ (¸.·' ☆ ABRA KADABRA...";
+    public static final String MAGICAL_LENNY = String.join("\n",
+            "（ ͡° ͜ʖ ͡°)つ━☆・。\n",
+            "⊂　　 ノ 　　　・゜+.\n",
+            "　しーＪ　　　°。+ ´¨)\n",
+            "　　　　　　　　　.· ´¸.·´¨) ¸.·*¨)\n",
+            "　　　　　　　　　　(¸.·´ (¸.·' ☆ ABRA KADABRA..."
+    );
 
-            public static String EAGLE_OF_LENNY =
-            ":＜￣｀ヽ、　　　　　　　／￣＞\n" +
-            "　ゝ、　　＼　／⌒ヽ,ノ 　/´\n" +
-            "　　　ゝ、　`（ ( ͡° ͜ʖ ͡°) ／\n" +
-            "　　 　　>　 　 　,ノ\n" +
-            "　　　　　∠_,,,/´”";
+    public static final String EAGLE_OF_LENNY = String.join("\n",
+            ":＜￣｀ヽ、　　　　　　　／￣＞\n",
+            "　ゝ、　　＼　／⌒ヽ,ノ 　/´\n",
+            "　　　ゝ、　`（ ( ͡° ͜ʖ ͡°) ／\n",
+            "　　 　　>　 　 　,ノ\n",
+            "　　　　　∠_,,,/´”"
+    );
 
-    public static final String DOG =
+    public static final String DOG = String.join("\n",
             //Dog created by @Ace#8925
-            "┈┈╱▏┈┈┈┈┈╱▔▔▔▔╲┈ \n" +
-            "┈┈▏▏┈┈┈┈┈▏╲▕▋▕▋▏ \n" +
-            "┈┈╲╲┈┈┈┈┈▏┈▏┈▔▔▔▆ ------- BORF!\n" +
-            "┈┈┈╲▔▔▔▔▔╲╱┈╰┳┳┳╯ \n" +
-            "╱╲╱╲▏┈┈┈┈┈┈▕▔╰━╯ \n" +
-            "▔╲╲╱╱▔╱▔▔╲╲╲╲┈┈┈ \n" +
-            "┈┈╲╱╲╱┈┈┈┈╲╲▂╲▂┈ \n" +
-            "┈┈┈┈┈┈┈┈┈┈┈╲╱╲╱┈\n" +
-            "Spread him to take over Discord!(Don't spam him though!)";
+            "┈┈╱▏┈┈┈┈┈╱▔▔▔▔╲┈ \n",
+            "┈┈▏▏┈┈┈┈┈▏╲▕▋▕▋▏ \n",
+            "┈┈╲╲┈┈┈┈┈▏┈▏┈▔▔▔▆ ------- BORF!\n",
+            "┈┈┈╲▔▔▔▔▔╲╱┈╰┳┳┳╯ \n",
+            "╱╲╱╲▏┈┈┈┈┈┈▕▔╰━╯ \n",
+            "▔╲╲╱╱▔╱▔▔╲╲╲╲┈┈┈ \n",
+            "┈┈╲╱╲╱┈┈┈┈╲╲▂╲▂┈ \n",
+            "┈┈┈┈┈┈┈┈┈┈┈╲╱╲╱┈\n",
+            "Spread him to take over Discord!(Don't spam him though!)"
+   );
 }
 
 //RIP DogCommand.java 2016-2017 "gib pats to me"
