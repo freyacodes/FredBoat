6204e1fa66b74a7668e89dfcc646c9d8c5d45447
Ruben Dijkstra <rubendijkstra123@gmail.com>
2017-10-01 18:47:58 +0200

    Switch to debug level logging

--- FredBoat/src/main/java/fredboat/agent/FredBoatAgent.java
+++ FredBoat/src/main/java/fredboat/agent/FredBoatAgent.java
@@ -68,9 +68,9 @@ public abstract class FredBoatAgent implements Runnable {
         LAST_RUN_TIME.put(this.getClass(), System.currentTimeMillis());
         try {
             Thread.currentThread().setName(name);
-            log.info("running");
+            log.debug("running");
             doRun();
-            log.info("done");
+            log.debug("done");
         } catch (Throwable t) {
             log.warn("Whoa! Unhandled throwable!", t);
         } finally {
