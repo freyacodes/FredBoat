284f825d206c79b0c19938aac588f14a02b326d9
Nanabell <marlon.delfin@googlemail.com>
2017-10-23 17:17:49 +0200

    Implement LEAVE_EMPTY_VC featureflag
    Moved the starting of the agent out of the music command initializer since we will merge the 2 anyways and insead of locking it behind a distribution made a featureflag for it,
    Usecase would be if you want to run the full bot. i.e dev disto but dont want the vc cleanup

--- FredBoat/src/main/java/fredboat/FredBoat.java
+++ FredBoat/src/main/java/fredboat/FredBoat.java
@@ -26,10 +26,7 @@
 package fredboat;
 
 import com.sedmelluq.discord.lavaplayer.tools.PlayerLibrary;
-import fredboat.agent.CarbonitexAgent;
-import fredboat.agent.DBConnectionWatchdogAgent;
-import fredboat.agent.FredBoatAgent;
-import fredboat.agent.StatsAgent;
+import fredboat.agent.*;
 import fredboat.api.API;
 import fredboat.audio.player.LavalinkManager;
 import fredboat.audio.queue.MusicPersistenceHandler;
@@ -150,6 +147,14 @@ public abstract class FredBoat {
             MainCommandInitializer.initCommands();
 
         MusicCommandInitializer.initCommands();
+
+        // The null check is to ensure we can run this in a test run
+        if (FeatureFlags.LEAVE_EMPTY_VC.isActive()) {
+            log.info("Starting VoiceChannelCleanupAgent.");
+            FredBoatAgent.start(new VoiceChannelCleanupAgent());
+        } else {
+            log.info("Skipped setting up the VoiceChannelCleanupAgent since its disabled in FeatureFlags.");
+        }
 
         log.info("Loaded commands, registry size is " + CommandRegistry.getSize());
 
