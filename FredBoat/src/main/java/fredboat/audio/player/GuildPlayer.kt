60153784dd47364bb2145862ad30c4ace82a4d59
Vincent Aymonin <vincent.aymonin@gmail.com>
2019-12-09 10:57:56 +0100

    Added VoteShuffleCommand

--- FredBoat/src/main/java/fredboat/audio/player/GuildPlayer.kt
+++ FredBoat/src/main/java/fredboat/audio/player/GuildPlayer.kt
@@ -30,7 +30,6 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack
 import fredboat.audio.lavalink.SentinelLavalink
 import fredboat.audio.queue.*
-import fredboat.command.music.control.VoteShuffleCommand
 import fredboat.command.music.control.VoteSkipCommand
 import fredboat.commandmeta.MessagingException
 import fredboat.commandmeta.abs.CommandContext
